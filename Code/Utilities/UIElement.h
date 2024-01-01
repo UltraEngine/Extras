32c564aebff48c5d3411888a91058296f4e33b1e
UltraEngine <64279130+UltraEngine@users.noreply.github.com>
2024-01-01 08:25:37 -0800

    Update UIElement.h

--- Code/Utilities/UIElement.h
+++ Code/Utilities/UIElement.h
@@ -1,19 +1,11 @@
 #pragma once
 #include "UltraEngine.h"
 
-class ObjectBar;
-namespace UltraEngine::Editor
-{
-    class Program;
-}
-
 namespace UltraEngine
 {
-    class UIElement : public Panel
+    class UIElement : public Object
     {
     protected:
-        friend UltraEngine::Editor::Program;
-        friend ObjectBar;
         static bool EventCallback(const Event& e, shared_ptr<Object> extra);
         virtual bool ProcessEvent(const Event& e);
         virtual void Listen(const EventId eventid = EVENT_NONE, shared_ptr<Object> widget = NULL);
